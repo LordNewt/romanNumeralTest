1d38deb97a8c7a386ae400268fb02b33fb36e9ec
Sam Schmit <sschmit@gmail.com>
2016-07-28 18:29:56 -0500

    Now it actually has to do something

--- test/romanConversionTest.py
+++ test/romanConversionTest.py
@@ -7,3 +7,6 @@ class RomanConversionTest(unittest.TestCase):
 
     def test_one_to_numeral(self):
         self.assertEqual("I", self.converter.toRomanNumeral(1))
+
+    def test_two_to_numeral(self):
+        self.assertEqual("II", self.converter.toRomanNumeral(2))
