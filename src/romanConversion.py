bcefbb0073b5702ffc8bef7552910d586458a676
Sam Schmit <sschmit@gmail.com>
2016-07-28 19:09:13 -0500

    Recognize all other numerals now.  Had to re-order the numeralValues dictionary due to Python not guaranteeing order.

--- src/romanConversion.py
+++ src/romanConversion.py
@@ -3,8 +3,13 @@ class RomanConversion():
     # Set up a map with the numeric value of each roman numeral. Listed
     # in descending value order.
     numeralValues = {
-        'V': 5,
-        'I': 1
+        1000: 'M"',
+        500: 'D',
+        100: 'C',
+        50: 'L',
+        10: 'X',
+        5: 'V',
+        1: 'I'
     }
 
     def toRomanNumeral(self, input):
@@ -13,16 +18,17 @@ class RomanConversion():
         # Add digits until the remaining value is reduced to zero
         while input > 0:
 
-            # Iterate over the numerals
-            for numeral,value in self.numeralValues.items():
+            # Iterate over the numerals. Python fights a little by not guaranteeing
+            # order on a dictionary. However, I have made use of .sorted() to fix this
+            for digit,numeral in sorted(self.numeralValues.items(), reverse=True):
                 # Don't attempt to evaluate if already at zero
                 if input > 0:
                     # If the remaining value is greater than the current
                     # numeral value, append that numeral to the output
                     # and reduce the remaining value
-                    while input >= value:
+                    while input >= digit:
                         outputStr += numeral
-                        input -= value
+                        input -= digit
 
         # Return the numeral string
         return outputStr
