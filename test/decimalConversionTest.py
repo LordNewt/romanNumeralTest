66b2c14cfd6097e7fb2e49cf4581967e4c7514a2
Sam Schmit <sschmit@gmail.com>
2016-07-28 19:45:06 -0500

    Beginning test for numeral to decimal converter

---
+++ test/decimalConversionTest.py
@@ -0,0 +1,12 @@
+import unittest
+from src.decimalConversion import DecimalConversion
+
+class DecimalConversionTest(unittest.TestCase):
+    def setUp(self):
+        self.converter = DecimalConversion()
+
+    #
+    # Basic digit tests
+    #
+    def test_i_to_number(self):
+        self.assertEqual(1, self.converter.toDecimalValue('I'))
