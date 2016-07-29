edfab6cf5a650f803728775795aa2d7c752b738f
Sam Schmit <sschmit@gmail.com>
2016-07-28 21:59:58 -0500

    Added console code to do either coversion.

--- converter.py
+++ converter.py
@@ -1,10 +1,22 @@
 from src.romanConversion import RomanConversion
+from src.decimalConversion import DecimalConversion
 
+#
+#   This script can be used to manually try out the decimal/numeral
+#   conversion capabilities.
+#
 exit = False
 romanConverter = RomanConversion()
+decimalConverter = DecimalConversion()
+
+# Keeps running until the user types 'exit'
 while not exit:
     user_input = input("Enter the number to convert: ")
     if user_input == 'exit':
         exit = True
     else:
-        print("Converted to {0}".format(romanConverter.toRomanNumeral(int(user_input))))
+        # If the user input a decimal, convert to numeral. Otherwise, covert to a decimal
+        if (user_input.isdigit()):
+            print("Converted to {0}".format(romanConverter.toRomanNumeral(int(user_input))))
+        else:
+            print("Converted to {0}".format(decimalConverter.toDecimalValue(user_input)))
