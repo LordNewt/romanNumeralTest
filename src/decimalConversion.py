31b6b0cd3a0b574b9fb8ac614d00064dc89df1b7
Sam Schmit <sschmit@gmail.com>
2016-07-28 19:54:55 -0500

    Added the rest of the required numerals to the conversion class

--- src/decimalConversion.py
+++ src/decimalConversion.py
@@ -1,6 +1,11 @@
 class DecimalConversion():
 
     decimalValues = {
+        'M': 1000,
+        'D': 500,
+        'C': 100,
+        'L': 50,
+        'X': 10,
         'V': 5,
         'I': 1
     }
