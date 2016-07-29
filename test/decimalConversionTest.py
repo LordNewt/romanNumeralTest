4ea4516e9ffe28929860e13f3446485b5c8c3965
Sam Schmit <sschmit@gmail.com>
2016-07-28 19:54:33 -0500

    Added the rest of the 'simple' single numeral tests

--- test/decimalConversionTest.py
+++ test/decimalConversionTest.py
@@ -13,3 +13,18 @@ class DecimalConversionTest(unittest.TestCase):
 
     def test_v_to_number(self):
         self.assertEqual(5, self.converter.toDecimalValue('V'))
+
+    def test_x_to_number(self):
+        self.assertEqual(10, self.converter.toDecimalValue('X'))
+
+    def test_l_to_number(self):
+        self.assertEqual(50, self.converter.toDecimalValue('L'))
+
+    def test_c_to_number(self):
+        self.assertEqual(100, self.converter.toDecimalValue('C'))
+
+    def test_d_to_number(self):
+        self.assertEqual(500, self.converter.toDecimalValue('D'))
+
+    def test_m_to_number(self):
+        self.assertEqual(1000, self.converter.toDecimalValue('M'))
