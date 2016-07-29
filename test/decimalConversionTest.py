60324a57f3b10768a82c8697a68bfcd6958df861
Sam Schmit <sschmit@gmail.com>
2016-07-28 19:51:02 -0500

    Added next digit test, so it has to actually check the digit

--- test/decimalConversionTest.py
+++ test/decimalConversionTest.py
@@ -10,3 +10,6 @@ class DecimalConversionTest(unittest.TestCase):
     #
     def test_i_to_number(self):
         self.assertEqual(1, self.converter.toDecimalValue('I'))
+
+    def test_v_to_number(self):
+        self.assertEqual(5, self.converter.toDecimalValue('V'))
