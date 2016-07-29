a01bfc5f8925a0ddb13b176f9a213bb860f97ab9
Sam Schmit <sschmit@gmail.com>
2016-07-28 19:54:07 -0500

    Added digit checking.

--- src/decimalConversion.py
+++ src/decimalConversion.py
@@ -1,4 +1,10 @@
 class DecimalConversion():
 
+    decimalValues = {
+        'V': 5,
+        'I': 1
+    }
+
     def toDecimalValue(self, input):
-        return 1
+        input = input.upper()
+        return self.decimalValues[input] if input in self.decimalValues else -1
